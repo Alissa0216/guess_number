bd1cdfbcd2974bb3c5599587d881301a325e150f
Alissa Lam <shiky5678@gmail.com>
2021-01-06 11:04:26 +0800

    update1

--- r.py
+++ r.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 # count = count + 1
 	num = input('guess number: ')
 	num = int(num)
 	if num == r:
 		print('you right!')
+		print('第', count, '次猜')
 		break
 	elif num > r:
 		print('over than the ans!')
 	elif num < r:
 		print('smaller than th ans!')
+	print('第', count, '次猜')
